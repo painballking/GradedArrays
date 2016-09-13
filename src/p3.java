27e2fcca8fd3199ebd345a211851a99e380ce099
Ryan Wright <painballking@gmail.com>
2016-09-13 10:23:38 -0400

    Initial commit

---
+++ src/p3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner();
+
+public class p3{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        int[] n = new int[input.nextInt()];
+        for(int i=0; i<n.length; i++){
+            n[i] = i+1;
+        }
+        int m = input.nextInt();
+
+
+        int deleted = 0;
+        int count;
+        int index = 0;
+
+        while(deleted != n.length-1){
+            count = 1;
+            while(count != m){
+                
+            }
+        }
+    }
+}
