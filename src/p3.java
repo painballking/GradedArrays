ea89725af5e9e9a23790951682d23a5bf6928da0
painb <painballking@gmail.com>
2016-09-14 22:15:28 -0400

    moved subtraction to its own method and created multiply method started problem 6

--- src/p3.java
+++ src/p3.java
@@ -3,37 +3,33 @@ import java.util.Scanner;
 public class p3{
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
-//        int[] n = new int[input.nextInt()];
-        int[] n = new int[6];
+        int[] n = new int[input.nextInt()];
         for(int i=0; i<n.length; i++){
             n[i] = i+1;
         }
-        //int m = input.nextInt();
-        int m = 5;
+        int m = input.nextInt();
 
         int deleted = 0;
-        int count;
         int index = 0;
-
+        int count = 0;
         while(deleted != n.length-1){
-            count = 1;
-            while(count != m){
-                if(n[index] != 0){
-                    count++;
-                    index++;
+            if(n[index] != 0){
+                count++;
+                if(count == m){
+                    n[index] = 0;
+                    deleted++;
+                    count = 0;
+                    for(int i:n)
+                        System.out.print(i+ " ");
+                    System.out.println();
                 }
-                while(index != n.length && n[index] == 0){
-                    index++;
-                }
-                if(index == n.length){
-                    index = 0;
-                }
+                index++;
             }
-            n[index] = 0;
-            deleted++;
-            for(int i:n)
-                System.out.print(i + " ");
-            System.out.println();
+            else
+                index++;
+            if(index == n.length){
+                index = 0;
+            }
         }
     }
 }
---
+++ src/p6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class p6 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        int[] unused = new int[9];
+        int[] input = new int[9];
+
+        for(int i=0; i<9; i++){
+
+        }
+    }
+}
