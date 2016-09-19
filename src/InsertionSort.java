054f715aa8cab046351aad1a08e657e686afe00e
Ryan Wright <painballking@gmail.com>
2016-09-19 10:24:59 -0400

    Finished insertion sort started radix sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,17 +1,23 @@
 public class InsertionSort {
     public static void main(String args[]) {
-        int[] array = {6,8,7,3,9,2,4,1,5};
+        int[] array = {6, 8, 7, 3, 9, 2, 4, 1, 5};
 
         int num;
+        int index;
         for(int i=1; i<array.length; i++){
             num = array[i];
-            for(int j=i-1; j!=0; j--){
-                if(array[j] > num){
-                    array[j+1] = array[j];
+            if(array[i-1] > num){
+                boolean nan = true;
+                for(int j=i; j>=1; j--){
+                    array[j] = array[j-1];
+                    if(array[j-1] < num){
+                        array[j] = num;
+                        nan = false;
+                        break;
+                    }
                 }
-                else{
-
-                }
+                if(nan)
+                    array[0] = num;
             }
             for(int x:array)
                 System.out.print(x+" ");
