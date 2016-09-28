f8886c1d7ee1bc887eadd53340817f3efa960a76
Ryan Wright <painballking@gmail.com>
2016-09-28 10:25:30 -0400

    idk and dont care enough to make good commit messages

--- src/LinkedLists/LinkedList.java
+++ src/LinkedLists/LinkedList.java
@@ -1,51 +1,104 @@
 package LinkedLists;
 
-public class LinkedList
-{
+public class LinkedList {
     private Node head;
     private Node tail;
 
-    public LinkedList()
-    {
+    public LinkedList() {
         head = null;
         tail = null;
     }
 
-    public void insertAtBack(int word)
-    {
-        if (head == null)
-        {
-            head = new Node(word, null);
+    public void insertAtBack(int value) {
+        if (head == null) {
+            head = new Node(value, null);
             tail = head;
         }
-        else
-        {
+        else {
             Node tempref = tail;
-            tail = new Node (word, null);
+            tail = new Node (value, null);
             tempref.setNextReference(tail);
         }
     }
 
-    public void printList()
-    {
-        if (head == null)
-        {
+    public void printList() {
+        if (head == null) {
             System.out.println ("The List is Empty");
         }
-        else
-        {
+        else {
             Node tempref = head;
+
             System.out.println("The values so far in the list : ");
-            while (tempref != null)
-            {
+            while (tempref != null) {
                 System.out.print(tempref.getData());
                 tempref = tempref.getNextReference();
-                if (tempref != null)
-                {
+                if (tempref != null) {
                     System.out.print (" --> ");
                 }
             }
             System.out.println("\n");
+        }
+    }
+
+    public boolean isEmpty(){
+        if(head == null){
+            return true;
+        }
+        return false;
+    }
+
+    public void insertAtFront(int input){
+        if(head == null){
+            head = new Node(input, null);
+            tail = head;
+        }
+        else{
+            Node tempref = head;
+            head = new Node(input, tempref);
+        }
+    }
+
+    public void removeFromFront(){
+        if(head != null){
+            head = head.getNextReference();
+        }
+    }
+
+    public void removeFromBack(){
+        if(head != null){
+            if(head == tail){
+                head = null;
+            }
+            else{
+                Node tempref = head;
+                while(tempref.getNextReference().getNextReference() != null){
+                    tempref = tempref.getNextReference();
+                }
+                tail = tempref;
+                tempref.setNextReference(null);
+            }
+        }
+    }
+
+    public void moveMax(){
+        if(head != null){
+            int maxValue = head.getData();
+            Node maxReference = head;
+            Node tempref = head;
+            while(tempref.getNextReference() != null){
+                if(tempref.getData() > maxValue){
+                    maxValue = tempref.getData();
+                    maxReference = tempref;
+                }
+                tempref = tempref.getNextReference();
+            }
+            if(maxReference != tail){
+                tempref = head;
+                Node refBeforeMaxRef = head;
+                while(tempref.getNextReference() != maxReference){
+                    tempref = tempref.getNextReference();
+                }
+            }
         }
     }
 
--- src/LinkedLists/LinkedListTest.java
+++ src/LinkedLists/LinkedListTest.java
@@ -8,7 +8,11 @@ public class LinkedListTest
     public static void Menu()
     {
         System.out.println ("1 - Insert At Back ");
-        System.out.println ("2 - Quit");
+        System.out.println ("2 - Insert At Front");
+        System.out.println ("3 - Remove From Front");
+        System.out.println ("4 - Remove From Back");
+        System.out.println ("5 - Move max to back");
+        System.out.println ("6 - Quit");
     }
 
 
@@ -18,44 +22,54 @@ public class LinkedListTest
         Scanner scanner = new Scanner (System.in);
         Scanner scanner1 = new Scanner (System.in);
 
+
         int choice = 0, status = 0, entry = 0;
         boolean flag = true;
         LinkedList list = new LinkedList();
 
-        while (flag)
-        {
-
+        while (flag) {
             Menu();
             status = 0;
 
-            try
-            {
+            try {
                 choice = Integer.parseInt(scanner.nextLine());
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 System.out.println("Integers Only - Enter Again");
                 status = 1;
             }
 
-            if (choice == 1)
-            {
+            if (choice == 1) {
                 System.out.println ("Enter the value?");
                 entry = scanner1.nextInt();
                 list.insertAtBack(entry);
             }
-            else if (choice == 2)
-            {
+
+            else if (choice == 2) {
+                System.out.println ("Enter the value?");
+                entry = scanner1.nextInt();
+                list.insertAtFront(entry);
+            }
+            else if (choice == 3) {
+                list.removeFromFront();
+            }
+
+            else if (choice == 4) {
+                list.removeFromBack();
+            }
+            else if(choice == 5){
+                list.moveMax();
+            }
+            else if (choice == 6) {
                 flag = false;
                 System.out.println ("End of Program");
             }
 
             else if (status == 0)
             {
-                System.out.println ("Invalid Entry - Enter Again");
+                System.out.println ("Invalid Entry - Choose From Menu");
             }
             list.printList();
-            System.out.println("\b");
         }
 
     }
