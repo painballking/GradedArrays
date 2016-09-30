00cf15e9d26442625545a5554fc5148f77477bfe
Ryan Wright <painballking@gmail.com>
2016-09-30 10:23:08 -0400

    k

--- src/LinkedLists/LinkedListTest.java
+++ src/LinkedLists/LinkedListTest.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class LinkedListTest {
 
-    public static void mainMenu(){
+    public static void printMainMenu(){
         System.out.println("1 - Insertion functions");
         System.out.println("2 - Removal functions");
         System.out.println("3 - Modifier functions");
@@ -13,40 +13,90 @@ public class LinkedListTest {
         System.out.println("0 - Quit");
     }
 
-    public static void insertMenu(){
+    public static boolean mainMenu(int choice, LinkedList list){
+        switch(choice){
+            case 1:
+                printInsertMenu();
+                while(!insertMenu(getInt(), list));
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public static void printInsertMenu(){
         System.out.println("1 - Insert at back");
         System.out.println("2 - Insert at front");
         System.out.println("3 - Insert based on value");
         System.out.println("0 - Back");
     }
 
-    public static void removeMenu(){
+    public static boolean insertMenu(int choice, LinkedList list){
+        switch(choice){
+            case 1: list.insertAtBack(getValue());
+            case 2: list.insertAtFront(getValue());
+            case 3: list.insertionPlace(getValue());
+                    return true;
+            default:
+                System.out.println("Invalid Entry. Try Again.");
+                return false;
+        }
+    }
+
+    public static void printRemoveMenu(){
         System.out.println("1 - Remove from back");
         System.out.println("2 - Remove from front");
         System.out.println("0 - Back");
     }
 
-    public static void modifierMenu(){
+    public static void printModifierMenu(){
         System.out.println("1 - Move max to back");
         System.out.println("2 - Bubble sort");
         System.out.println("0 - Back");
     }
 
+    public static int getValue(){
+        System.out.print("Enter the value: ");
+        boolean gotGoodValue = false;
+        int choice = 0;
+        while(!gotGoodValue) {
+            gotGoodValue = true;
+            try {
+                choice = Integer.parseInt(new Scanner(System.in).nextLine());
+            } catch (Exception e) {
+                System.out.println("Integers only. Try again.");
+                gotGoodValue = false;
+            }
+        }
+        return choice;
+    }
+
+    public static int getInt(){
+        boolean gotGoodValue = false;
+        int choice = 0;
+        while(!gotGoodValue) {
+            gotGoodValue = true;
+            try {
+                choice = Integer.parseInt(new Scanner(System.in).nextLine());
+            } catch (Exception e) {
+                System.out.println("Integers only. Try again.");
+                gotGoodValue = false;
+            }
+        }
+        return choice;
+    }
+
     public static void main (String args []) {
         Scanner input = new Scanner(System.in);
+        int status, choice = 0;
 
         ArrayList<LinkedList> lists = new ArrayList<LinkedList>();
+        LinkedList list = new LinkedList();
 
         while(true){
-            mainMenu();
-            int choice = input.nextInt();
-
-            switch(choice){
-                case 1:
-                    Scanner temp = new Scanner(System.in);
-                    insertMenu();
-                    switch
-            }
+            printMainMenu();
+            mainMenu(getInt(), list);
+            list.printList();
         }
     }
 }
