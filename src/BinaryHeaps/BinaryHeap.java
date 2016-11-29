ae77b6c016d723a45a529d2b58ff4147673779f5
painb <painballking@gmail.com>
2016-11-28 22:57:02 -0500

    did some things

--- src/BinaryHeaps/BinaryHeap.java
+++ src/BinaryHeaps/BinaryHeap.java
@@ -1,33 +1,51 @@
 package BinaryHeaps;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 public class BinaryHeap {
-    static LinkedList<Integer> heap = new LinkedList<>();
 
-    static public void insert(int value){
-        heap.add(value);
-        int current_cell = heap.size()-1;
-        while(current_cell != 0 && heap.get((current_cell-1)/2) > heap.get(current_cell)){
-            int parent_val = heap.get((current_cell-1)/2);
-            heap.set((current_cell-1)/2, heap.get(current_cell));
-            heap.set(current_cell, parent_val);
-            current_cell = (current_cell-1)/2;
+    public static class Heap{
+
+        private List<Integer> heap;
+
+        public Heap(){ heap = new ArrayList<>(); }
+
+        public void insert(int value){
+            heap.add(value);
+            int current_cell = heap.size()-1;
+            while(current_cell != 0 && heap.get((current_cell-1)/2) > heap.get(current_cell)){
+                int parent_val = heap.get((current_cell-1)/2);
+                heap.set((current_cell-1)/2, heap.get(current_cell));
+                heap.set(current_cell, parent_val);
+                current_cell = (current_cell-1)/2;
+            }
         }
-    }
 
-    static public void print(LinkedList list){
-        System.out.println(Arrays.toString(list.toArray()));
+        public void print(){
+            System.out.println(Arrays.toString(heap.toArray()));
+        }
+
+        public int pop(){
+            int temp = heap.get(0);
+            heap.set(0, heap.get(heap.size()-1));
+            heap.remove(heap.size()-1);
+            return temp;
+        }
     }
 
     public static void main(String args[]){
         int[] array = {2,4,11,15,16,18};
+        Heap myHeap = new Heap();
         for(int i:array){
-            heap.add(i);
+            myHeap.insert(i);
         }
-        print(heap);
-        insert(1);
-        print(heap);
+        myHeap.print();
+        myHeap.insert(7);
+        myHeap.print();
+        myHeap.insert(12);
+        myHeap.print();
     }
 }
---
+++ src/VCU/Problem_D.java
@@ -0,0 +1,64 @@
+package VCU;
+
+import java.util.*;
+
+public class Problem_D {
+
+    //Test if the cross product yields a z axis positive vector
+    public static boolean crossIsPos(double[] a, double[] b){
+        return a[0]*b[1] - a[1]*b[0] >= 0;
+    }
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+
+        //Parse triangle points
+        double[][] triPoints = new double[3][3];
+        for(int i=0; i<3; i++){
+            for(int j=0; j<2; j++){
+                triPoints[i][j] = scan.nextDouble();
+            }
+        }
+
+        //Calculate triangle edge vectors
+        double[][] edgeVectors = new double[3][3];
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                edgeVectors[i][j] = triPoints[(i+1)%3][j] - triPoints[i][j];
+            }
+        }
+
+        //Parse points to be tested
+        int numPoints = scan.nextInt();
+        double[][] points = new double[numPoints][3];
+        for(int i=0; i<numPoints; i++){
+            for(int j=0; j<2; j++){
+                points[i][j] = scan.nextDouble();
+            }
+        }
+
+        //For each point
+        for(int x=0; x<numPoints; x++){
+
+            //Calculate the triangle - floating point vectors
+            double[][] pointVectors = new double[3][3];
+            for(int i=0; i<3; i++){
+                for(int j=0; j<3; j++){
+                    pointVectors[i][j] = points[x][j]-triPoints[i][j];
+                }
+            }
+
+            //Compare vector orientation
+            boolean state = crossIsPos(edgeVectors[0], pointVectors[0]);
+            boolean flag = false;
+            for(int i=1; i<3; i++){
+                if(crossIsPos(edgeVectors[i], pointVectors[i]) != state){
+                    flag = true;
+                    break;
+                }
+            }
+            System.out.print(flag ? "0" : "1");
+        }
+
+    }
+}
