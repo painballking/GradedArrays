84906ee79717f837b669af710e87d91467ca6383
painb <painballking@gmail.com>
2016-12-12 22:59:31 -0500

    did some things

--- src/BinaryHeaps/BinaryHeap.java
+++ src/BinaryHeaps/BinaryHeap.java
@@ -1,5 +1,7 @@
 package BinaryHeaps;
 
+import org.jetbrains.annotations.Contract;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -13,18 +15,22 @@ public class BinaryHeap {
 
         public Heap(){ heap = new ArrayList<>(); }
         public Heap(List<Integer> heap){
-            heap = heap;
+            heap = new ArrayList<Integer>(heap);
         }
 
         public void insert(int value){
             heap.add(value);
             int current_cell = heap.size()-1;
             while(current_cell != 0 && heap.get((current_cell-1)/2) > heap.get(current_cell)){
-                int parent_val = heap.get((current_cell-1)/2);
-                heap.set((current_cell-1)/2, heap.get(current_cell));
-                heap.set(current_cell, parent_val);
+                swap((current_cell-1)/2, current_cell);
                 current_cell = (current_cell-1)/2;
             }
+        }
+
+        public void swap(int index1, int index2){
+            int store = heap.get(index1);
+            heap.set(index1, heap.get(index2));
+            heap.set(index2, store);
         }
 
         public void print(){
@@ -36,29 +42,26 @@ public class BinaryHeap {
             heap.set(0, heap.get(heap.size()-1));
             heap.remove(heap.size()-1);
 
-            //Percolation Group
-            int p = 0; //Pointer to percolated
+            int p = 0;
             while( (p+1)*2 < heap.size() || p*2+1 < heap.size() ){
 
-                if ( (p+1)*2 >= heap.size() || p*2+1 >= heap.size()) {
-                    if((p+1)*2 >= heap.size() && heap.get(p*2+1) < heap.get(p)) {
-                        int store = heap.get(p*2+1);
-                        heap.set(p*2+1, heap.get(p));
-                        heap.set(p, store);
+                int leftChild = p*2+1;
+                int rightChild = (p+1)*2;
+
+                if ( leftChild >= heap.size() || rightChild >= heap.size()) {
+
+                    if(leftChild >= heap.size() && heap.get(rightChild) < heap.get(p)) {
+                        swap(rightChild, p);
                     }
-                    else if(p+1*2 >= heap.size() && heap.get((p*2)+1) < heap.get(p)) {
-                        int store = heap.get((p + 1) * 2);
-                        heap.set((p + 1) * 2, heap.get(p));
-                        heap.set(p, store);
+                    else if(rightChild >= heap.size() && heap.get(leftChild) < heap.get(p)) {
+                        swap(leftChild, p);
                     }
                     break;
                 }
                 else {
-                    if (heap.get((p+1)*2) < heap.get(p) || heap.get(p*2+1) < heap.get(p)) {
-                        int child = heap.get((p+1)*2) < heap.get(p*2+1) ? (p+1)*2 : p*2+1;
-                        int store = heap.get(child);
-                        heap.set(child, heap.get(p));
-                        heap.set(p, store);
+                    if (heap.get(leftChild) < heap.get(p) || heap.get(rightChild) < heap.get(p)) {
+                        int child = heap.get(leftChild) < heap.get(rightChild) ? leftChild : rightChild;
+                        swap(child, p);
                         p = child;
                     }
                     else{
@@ -66,23 +69,44 @@ public class BinaryHeap {
                     }
                 }
             }
-
             return temp;
         }
     }
 
-    public static ArrayList<Integer> heapify(ArrayList<Integer> list){
-        for(int i=(list.size()-2)/2; i>=0; i++){
-            break;
+    public static int[] heapify(int[] array){
+        for(int i=(array.length-1)/2; i>=0; i--){
+            if ( (i+1)*2 >= array.length || i*2+1 >= array.length) {
+                if((i+1)*2 >= array.length && array[i*2+1] < array[i]) {
+                    int store = array[i*2+1];
+                    array[i*2+1] = array[i];
+                    array[i] = store;
+                }
+                else if(i*2+1 >= array.length && array[(i*2)+1] < array[i]) {
+                    int store = array[(i+1)*2];
+                    array[(i+1)*2] = array[i];
+                    array[i] = store;
+                }
+                break;
+            }
+            else {
+                if (array[(i+1)*2] < array[i] || array[i*2+1] < array[i]) {
+                    int child = array[(i+1)*2] < array[i*2+1] ? (i+1)*2 : i*2+1;
+                    int store = array[child];
+                    array[child] = array[i];
+                    array[i] = store;
+                }
+            }
         }
-        return list;
-    }
-
-    public static void setHeap(ArrayList<Integer> heap){
-        heap = heap;
+        return array;
     }
 
     public static void main(String args[]){
-        int[] array = {5, 7, 10, 9, 11, 21};
+        int[] array = {21, 11, 9, 10, 7, 5};
+        array = heapify(array);
+        Heap myHeap = new Heap();
+        for(int i : array){
+            myHeap.insert(i);
+        }
+        myHeap.print();
     }
 }
