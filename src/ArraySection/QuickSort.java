be75c38f83b584b6ecab28353ce53c6a8c101da9
Ryan Wright <painballking@gmail.com>
2016-12-15 10:20:52 -0500

    k

--- src/ArraySection/QuickSort.java
+++ src/ArraySection/QuickSort.java
@@ -1,8 +1,33 @@
 package ArraySection;
 
+import java.util.Arrays;
+
 public class QuickSort {
 
-    public static void main(String args[]) {
+    public static void partition(int[] array, int a, int b){
 
+        int p = array[a];
+        int i = 1;
+        int j= array.length-1;
+
+        while(i >= j){
+            if(array[i] > p && array[j] < p){
+                int store = array[i];
+                array[i] = array[j]; array[j] = store;
+                i++; j--;
+            }
+            else if(array[i] <= p){
+                i++;
+            }
+            else if(array[j] >= p){
+                j--;
+            }
+        }
+        array
+    }
+
+    public static void main(String args[]) {
+        int[] test = {1, 2, 3, 4, 5};
+        partition(test);
     }
 }
